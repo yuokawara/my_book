169d84d4a06a3b6c044613abec86d26ae7d6982c
大河原悠 <solbonta1@gmail.com>
2019-11-20 10:51:24 +0900

    first commit

---
+++ app/Http/Controllers/Admin/BookController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function add()
+    {
+        return view('admin.book.create');
+    }
+}
